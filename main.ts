bb811300eef352878bc4b9a7aaad34a9320e2d09
Hernan Y.Ke <yuriqiao@gmail.com>
2016-12-18 14:01:47 -0500

    Observable

--- main.ts
+++ main.ts
@@ -1,1 +1,21 @@
-alert("x")
+import {Observable} from "rxjs";
+
+let numbers = [1,5,10];
+
+let source = Observable.from(numbers);
+
+class MyObserver{
+	next(v){
+		console.log(`value:${v}`);
+	}
+
+	error(e){
+		console.log(`error ${e}`);
+	}
+
+	complete(){
+		console.log("complete");
+	}
+}
+
+source.subscribe(new MyObserver());
