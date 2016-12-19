44705ed2d23881a450ee215c377b80fe3b6fb03c
Hernan Y.Ke <yuriqiao@gmail.com>
2016-12-19 16:54:54 -0500

    Unsubscribing for Cleanup

--- loader.ts
+++ loader.ts
@@ -3,7 +3,7 @@ import {Observable,Observer} from "rxjs";
 export function load(url:string){
 	return Observable.create(observer=>{
 			let xhr = new XMLHttpRequest();
-	xhr.addEventListener("load",()=>{
+	let onLoad = () =>{
 		if(xhr.status===200){
 		let data = JSON.parse(xhr.responseText);
 		observer.next(data);
@@ -11,15 +11,30 @@ export function load(url:string){
 		}else{
 			observer.error(xhr.statusText);
 		}
-	})
+	};
+	xhr.addEventListener("load",onLoad);
 	xhr.open("GET",url);
 	xhr.send();
+
+
+	return ()=>{
+		xhr.removeEventListener("load",onLoad);
+		xhr.abort();
+	}
 	}).retryWhen(retryStrategy())
 };
 //retry(3)
 
 export function loadWithFetch(url:string){
-	return Observable.fromPromise(fetch(url).then(r=>r.json()));
+	return Observable.fromPromise(fetch(url).then(r=>
+	if(r.status===200){
+	return r.json()
+}else{
+	return Promise.reject(r);
+}
+	)
+
+	);
 }
 
 function retryStrategy(){
--- main.ts
+++ main.ts
@@ -18,28 +18,28 @@ let numbers = [1,5,10];
 }).map(n=>n*2).filter(n=>n>4);*/
 
 //let circle = document.getElementById("circle")
-/*let button = document.getElementById("button");
+let button = document.getElementById("button");
 let output = document.getElementById("output");
-let click = Observable.fromEvent(button,"click");*/
+let click = Observable.fromEvent(button,"click");
 
 
 
 
-/*function renderMovies(movies){
+function renderMovies(movies){
 movies.forEach(m=>{
 			let div = document.createElement("div");
 			div.innerText = m.title;
 			output.appendChild(div);
 		})
-}*/
+}
 
 //click.map(e=>load("movies.json")).subscribe(o=>console.log(o));
 	
-/*click.flatMap(e=>loadWithFetch("movies.json")).subscribe(renderMovies,e=>{
+click.flatMap(e=>loadWithFetch("movies.json")).subscribe(renderMovies,e=>{
 		console.log(`error ${e}`);
 	},()=>{
 		console.log("complete");
-	});*/
+	});
 
 
 
